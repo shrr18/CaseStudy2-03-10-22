2668c0aa9220b172887bd001942779293b733043
shrr18 <113331370+shrr18@users.noreply.github.com>
2022-10-03 15:49:21 +0530

    Add files via upload

---
+++ OrderedItem.java
@@ -0,0 +1,108 @@
+package com.gl.CaseStudy2;
+
+public class OrderedItem {
+	 private String itemName;
+
+	 private String rate;
+
+	 private String orderQty;
+
+	 private String discountAmount;
+
+	 private String payAmount;
+
+	 public OrderedItem() {
+
+	 super();
+
+	 // TODO Auto-generated constructor stub
+
+	 }
+
+	 public OrderedItem(String itemName, String rate, String orderQty, String discountAmount, String payAmount) {
+
+	 super();
+
+	 this.itemName = itemName;
+
+	 this.rate = rate;
+
+	 this.orderQty = orderQty;
+
+	 this.discountAmount = discountAmount;
+
+	 this.payAmount = payAmount;
+
+	 }
+
+	 public String getItemName() {
+
+	 return itemName;
+
+	 }
+
+	 public void setItemName(String itemName) {
+
+	 this.itemName = itemName;
+
+	 }
+
+	 public String getRate() {
+
+	 return rate;
+
+	 }
+
+	 public void setRate(String rate) {
+
+	 this.rate = rate;
+
+	 }
+
+	 public String getOrderQty() {
+
+	 return orderQty;
+
+	 }
+
+	 public void setOrderQty(String orderQty) {
+
+	 this.orderQty = orderQty;
+
+	 }
+
+	 public String getDiscountAmount() {
+
+	 return discountAmount;
+
+	 }
+
+	 public void setDiscountAmount(String discountAmount) {
+
+	 this.discountAmount = discountAmount;
+
+	 }
+
+	 public String getPayAmount() {
+
+	 return payAmount;
+
+	 }
+
+	 public void setPayAmount(String payAmount) {
+
+	 this.payAmount = payAmount;
+
+	 }
+
+	 @Override
+
+	 public String toString() {
+
+	 return "OrderedItem [itemName=" + itemName + ", rate=" + rate + ", orderQty=" + orderQty + ", discountAmount="
+
+	  + discountAmount + ", payAmount=" + payAmount + "]";
+
+	 }
+
+	}
